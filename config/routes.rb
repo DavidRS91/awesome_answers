33bb205bf090d821ec88b21f6baad36b680be739
David Scott <david.r.scott91@gmail.com>
2018-02-09 09:21:48 -0800

    Add CRUD functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  get('/questions/new', to: 'questions#new', as: :new_question)
+  post('/questions', to: 'questions#create', as: :questions)
+  get('/questions', to: 'questions#index')
+  get('/questions/:id', to: 'questions#show', as: :question)
+  get('/questions/:id/edit', to: 'questions#edit', as: :edit_question)
+  patch('/questions/:id', to: 'questions#update')
+  put('/questions/:id', to: 'questions#update')
+  delete('/questions/:id', to: 'questions#destroy')
+
+  get('/',{to: 'welcome#index', as: :root})
+
+  get('/about',{to: 'welcome#about'})
+
+  get('/contact/new', {to: 'contact#new'})
+  post('contact/submit', {to: 'contact#create'})
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ Question.destroy_all
+
+ 100.times.each do
+   Question.create(
+     title: Faker::Movie.quote,
+     body: Faker::Lorem.paragraph,
+     view_count: rand(0..5000)
+   )
+ end
+
+ questions = Question.all
+
+ puts Cowsay.say "Created #{questions.count} questions", :ghostbusters
